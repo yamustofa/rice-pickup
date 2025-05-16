c7c0c1ff1e39766d7b11ea036845c17bac4467da
xhabibmustofax <xhabibmustofax@gmail.com>
2025-05-16 17:39:46 +0700

    fix: trying fix deploy

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    // @ts-ignore-next-line
-    instrumentationHook: true,
-  },
 };
 
 export default nextConfig;
